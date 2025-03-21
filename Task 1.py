adf2572e49a05884d80a55173b84611c3c38aafc
venkkateshan-57 <vvenkkateshan@gmail.com>
2025-03-21 21:44:02 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,6 @@
+n1=float(input("Enter number 1: "))
+n2=float(input("Enter number 2: "))
+print(f"Addition:{n1+n2}")
+print(f"Subtraction:{n1-n2}")
+print(f"Multiplication:{n1*n2}")
+print(f"Division:{n1/n2}")
---
+++ Task 2.py
@@ -0,0 +1,3 @@
+name=input("Enter your First name: ")
+last=input("Enter your Last name: ")
+print(f"Hello {name} {last}! Welcome to python programming")
