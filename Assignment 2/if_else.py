957f4eaa18de42cf51093723172fc4c6eda9cc7a
venkkateshan-57 <vvenkkateshan@gmail.com>
2025-03-21 22:22:50 +0530

    Add files via upload

---
+++ Assignment 2/if_else.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number: "))
+if num%2==0:
+    print(f"The entered number{num} is even")
+else:
+    print(f"The entered number {num} is odd")
---
+++ Assignment 2/loop.py
@@ -0,0 +1,4 @@
+j=0
+for i in range(1,51):
+    j+=i
+print(f"The total sum is {j}")
